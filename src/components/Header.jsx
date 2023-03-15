e9449a37b5b35a41a9d6fae5b07891c649b67f1d
siddhartha verma <siddharthaverma6213@gmail.com>
2023-03-15 13:14:09 +0530

    2nd commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,11 @@ export default function Header() {
     <div id='ele1'><h2>SidMeals</h2></div>
     <div id='ele2'><b> &nbsp; <AiOutlineShoppingCart/> </b> &nbsp;&nbsp; Your Cart &nbsp;<b id='num'>&nbsp; 0 &nbsp; </b> </div>
     </div>
-    <div id='image'><img src={bg} alt="imgError" id='img' /></div>
+    <div id='body1'>
+        <div id='img'><img src={bg} alt="imgError"/></div>
+        <div id='summary'><p id='para'><h1>Delicious Food, For You </h1><p> Lorem ipsum dolor sit amet consectes veritatis unde quidem nostrum molestiae facilis quas iure recusandae? </p> <p>Lorem ipsum dolor r amet accusantium modi debitis minus ipsam cumque ab eos asperiores animi!</p> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate placeat magni nihil is vel.</p></p> </div>
+    </div>
+    
     </>
   )
 }
